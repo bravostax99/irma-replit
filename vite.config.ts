7d9254725f1803f8f4a23e3454c9e14229794d96
bravostax99 <benny.bravo99@gmail.com>
2025-02-03 07:36:31 +0000

    replit update

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['d15b3307-c68a-4020-8563-e0cb82a897cd-00-2pf64zdqvpouq.pike.replit.dev']
+  }
 })
